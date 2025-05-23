85ab2e18da5650d9aa21d03cfe65ea8be83cdca4
rominabakulic <romina.bakulic@uc.cl>
2025-05-23 08:00:17 -0400

    subida update creds

--- src/gsheet.py
+++ src/gsheet.py
@@ -1,16 +1,20 @@
 import gspread
-from google.oauth2.service_account import Credentials
 from typing import List
+import google.auth            #
+
+
 
 def guardar_en_gsheet(posts_data: List[dict]) -> str:
 
     spreadsheet_id = "1-NQtPvk6Knd7CHH7XjyCWxeSmxltc5-lCg4SWF__RcI"
 
-    scope = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
-    creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
+    SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+    creds, _ = google.auth.default(scopes=SCOPES)
+
     client = gspread.authorize(creds)
 
     sheet = client.open_by_key(spreadsheet_id)
@@ -18,19 +22,20 @@ def guardar_en_gsheet(posts_data: List[dict]) -> str:
 
     ws.clear()
 
-     # Encabezados
-    rows = [["Titular", "Categoría", "Autor", "Tiempo de lectura", "Fecha de modificación", "URL"]]
+    # Encabezados
+    rows = [["Titular", "Categoría", "Autor",
+                "Tiempo de lectura", "Fecha de modificación", "URL"]]
 
     # Agregar datos
     for post in posts_data:
-        rows.append([
-            post.get("titular"),
-            post.get("categoria"),
-            post.get("autor"),
-            post.get("tiempo_lectura"),
-            post.get("fecha_publicacion"),
-            post.get("url"),
-        ])
+            rows.append([
+                post.get("titular"),
+                post.get("categoria"),
+                post.get("autor"),
+                post.get("tiempo_lectura"),
+                post.get("fecha_publicacion"),
+                post.get("url"),
+            ])
 
     # Cargar todos los datos en una sola llamada
     ws.update(f"A1:F{len(rows)}", rows)
